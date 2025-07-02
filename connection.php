e260495d9225bfd6d00875bd85dfe18cfc83aee4
Oleg KOlesnykov <kolleh1660@gmail.com>
2025-07-02 10:54:42 +0200

    add files

---
+++ connection.php
@@ -0,0 +1,9 @@
+<form action="connexion.php" method="post">
+  <label for="identifiant">Nom d'utilisateur :</label>
+  <input type="text" id="identifiant" name="identifiant" required>
+
+  <label for="motdepasse">Mot de passe :</label>
+  <input type="password" id="motdepasse" name="motdepasse" required>
+
+  <button type="submit">Se connecter</button>
+</form>
