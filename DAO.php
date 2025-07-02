d4acffbe2d30d08e56cd48f8812938165c9ebada
Oleg KOlesnykov <kolleh1660@gmail.com>
2025-07-02 17:39:15 +0200

    update

--- DAO.php
+++ DAO.php
@@ -1,10 +1,10 @@
 <?php
     class DAOStock{
-        private $host="localhost";
-	private $dbname="stock";
-	private $username="root";
-	private $password="";
-	private $dbh;
+    	private $host="localhost";
+		private $dbname="gestion-des-stocks";
+		private $username="root";
+		private $password="";
+		private $dbh;
 
 	public function __construct() {
 		
@@ -13,7 +13,7 @@
 	public function connection() {
 		try {
 			$this->dbh = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname, $this->username, $this->password);
-			print "Connexion réussie";
+			// print "Connexion réussie";
 		} catch (PDOException $e) {
 			print $e->getMessage();
 			// tenter de réessayer la connexion après un certain délai, par exemple
@@ -21,8 +21,40 @@
 		}
 	}
 
+	public function getUsers(){
+		$users=$this->dbh->prepare("SELECT user_name FROM users ORDER BY user_name;");
+		$users->execute();
+		return $users->fetchALL(PDO::FETCH_ASSOC);
+	}
 
+	public function checkLogin($username,$password){
+		$sql="SELECT * FROM users WHERE user_name=?";
+		$stmt=$this->dbh->prepare($sql);
+		$stmt->execute([$username]);
+		$user=$stmt->fetch(PDO::FETCH_ASSOC);
+
+		if($user&& password_verify($password, $user['pwd'])){
+			return $user;
+		}else{
+			return false;
+		}
+	}
+
+	public function getPwds(){
+		$pwds=$this->dbh->prepare("SELECT pwd FROM users ORDER BY user_name;");
+		$pwds->execute();
+		return $pwds->fetchALL(PDO::FETCH_ASSOC);
+	}
+
+	public function getReserves(){
+		$sql= "SELECT reserve_name as reserve, color FROM reserves ORDER BY id_reserve;";
+		$getColor = $this->dbh->prepare($sql);
+		$getColor->execute();
+		return $getColor->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function deconnection() {
+		$this->dbh=null;
+	}
     }
-
-
 ?>
--- connection.php
+++ connection.php
@@ -1,5 +1,33 @@
 
-<form action="connexion.php" method="post">
+<?php
+session_start(); //debut de session
+
+require_once("dao.php"); //se connecte au DB
+$dao=new DAOStock();
+$dao->connection(); 
+
+$login=$_POST['identifiant']?? ''; //recouperation des donnees du "form"
+$password=$_POST['motdepasse']?? '';
+
+$user=$dao->checkLogin($login,$password);
+
+
+if($user && password_verify($password, $user['password_hash'])){
+  //sauvegarde des donnees d'user dans la session
+  $_SESSION['username']=$user['username'];
+  $_SESSION['role']=$user['role'] ?? 'user';
+
+  //redirection de vers "index.php"
+  header("Location: index.php");
+  exit;
+}else{
+  print "Nom d'utilisateur ou mot de passe incorrect.";
+}
+?>
+
+<?php require_once("header.php"); ?>
+
+<form action="connection.php" method="post">
   <label for="identifiant">Nom d'utilisateur :</label>
   <input type="text" id="identifiant" name="identifiant" required>
 
@@ -8,3 +36,5 @@
 
   <button type="submit">Se connecter</button>
 </form>
+
+<?php require_once("footer.php"); ?>
