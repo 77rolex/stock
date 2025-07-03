3c388f93a57ee2513c6ec8ae7758cccd5de05c57
Oleg KOlesnykov <kolleh1660@gmail.com>
2025-07-03 10:31:05 +0200

    update

--- connection.php
+++ connection.php
@@ -1,40 +1,48 @@
-
 <?php
 session_start(); //debut de session
+if ($_POST) {
+  require_once("dao.php"); //se connecte au DB
+  $dao=new DAOStock();
+  $dao->connection(); 
 
-require_once("dao.php"); //se connecte au DB
-$dao=new DAOStock();
-$dao->connection(); 
+  $login=$_POST['exampleInputEmail1']?? ''; //recouperation des donnees du "form"
+  $password=$_POST['exampleInputPassword1']?? '';
 
-$login=$_POST['identifiant']?? ''; //recouperation des donnees du "form"
-$password=$_POST['motdepasse']?? '';
+  $user=$dao->checkLogin($login,$password);
 
-$user=$dao->checkLogin($login,$password);
+  if($user){
+      //sauvegarde des donnees d'user dans la session
+      $_SESSION['username']=$user['username'];
+      $_SESSION['role']=$user['role'] ?? 'user';
 
-
-if($user && password_verify($password, $user['password_hash'])){
-  //sauvegarde des donnees d'user dans la session
-  $_SESSION['username']=$user['username'];
-  $_SESSION['role']=$user['role'] ?? 'user';
-
-  //redirection de vers "index.php"
-  header("Location: index.php");
-  exit;
-}else{
-  print "Nom d'utilisateur ou mot de passe incorrect.";
+      //redirection de vers "index.php"
+      header("Location: index.php");
+      exit;
+    }else{?>
+        <script>
+          alert("Nom d'utilisateur ou mot de passe incorrect.");
+        </script>
+    <?php } 
 }
 ?>
 
 <?php require_once("header.php"); ?>
+<main>
 
-<form action="connection.php" method="post">
-  <label for="identifiant">Nom d'utilisateur :</label>
-  <input type="text" id="identifiant" name="identifiant" required>
+  <form action="connection.php" method="post">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Nom d'utilisateur :</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="emailHelp" required>
+      <small class="form-text text-muted">Mettez votre nom d'utilisateur</small>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Mot de passe :</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name="exampleInputPassword1">
+      <small class="form-text text-muted">Mettez votre mot de passe</small>
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+  </form>
 
-  <label for="motdepasse">Mot de passe :</label>
-  <input type="password" id="motdepasse" name="motdepasse" required>
-
-  <button type="submit">Se connecter</button>
-</form>
-
+</main>
 <?php require_once("footer.php"); ?>
