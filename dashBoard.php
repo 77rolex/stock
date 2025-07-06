779315c0a99c0805f8290cf64019e053f53d77c2
Siva <sivasandraradj@gmail.com>
2025-07-06 18:10:39 +0200

    update with filter

--- dashBoard.php
+++ dashBoard.php
@@ -1,5 +1,3 @@
-<?php
-
 // Connexion a la base de donnees avec gestion des erreurs
 // username = formateur, pwd = formation
 require_once("DAO.php");
@@ -8,10 +6,10 @@ $dao->connection();
 
 // Le formateur de son stock par un tableau de bord.
 // Ce tbord contiendra tous les matériels (nom, unité, quantité), la localisation (la réserve) identifiable par couleur.
-$produits=$dao->getProduits();
+$produits = $dao->getProduits();
  
 // recupere la valeur saisie dans searchbar ou affiche vide
-$produits=$dao->getSearchbar();
+$produits = $dao->getSearchbar();
 
 ?>
 
@@ -26,13 +24,16 @@ $produits=$dao->getSearchbar();
     tr, td
     .reserve_color {
         display: inline-block;
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        margin-right: 6px;
     }
 
     .alerte {
         background-color: #ff6347;
     }
 </style>
-
 <!-- search bar -->
 <form method="get">
     <input type="text" name="search" placeholder="Des recherches par matériel (saisie texte) ou par référence seront possibles." value="">
@@ -48,31 +49,30 @@ $produits=$dao->getSearchbar();
         <th>Nom du produit</th>
         <th>Unité</th>
         <th>Qty</th>
-        <th>Couleur</th>
         <th>Reserve</th>
         <th>Category</th>
         <th>Alerte</th>
     </tr>
     </thead>
     <tbody>
-        <?php $seuil = $_GET["seuil"] ?? null ?>
+        <?php $seuil = $_GET["seuil"] ?? null; ?>
         <?php foreach($produits as $row){ ?>
-            <?php $alerte = ($seuil !== null && is_numeric($seuil) && $row["qt"] < $seuil); ?>
-        <tr class="<?php $alerte ? "alerte" : "" ; ?>">
+            <?php $alerte = ($seuil !== null && is_numeric($seuil) && $row["qt"] <= $seuil); ?> <!-- seuil defini && est un number && qt trop bas si conditions is true = $alerte -->
+        <tr class="<?php $alerte ? "alerte" : "" ; ?>"> <!-- $alerte = true = class .alerte = color or non -->
         <td><?php print $row["nom_produit"];?></td>
         <td><?php print $row["unite"];?></td>
         <td><?php print $row["qt"];?></td>
-        <td><div class="reserve_color" style="background-color:<?php print $row["color"];?>;"></div>
-        <?php print $row["reserve_name"];?></td>
+        <td><div class = "reserve_color" style="background-color:<?php print $row["color"];?>;"></div>
+        <?php print $row["reserve_name"];?>
+        </td>
         <td><?php print $row["nom_category"];?></td>
-        <td><div style="background-color:<?php ($row["qt"] <= 10) ? 'orange' : 'vert';?>"></div></td>
+        <td><div class = "alerte" style = "background-color:<?php ($row["qt"] <= $seuil) ;?>"></div></td>
     </tr>
     <?php } ?>
-       
+
     </tbody>
 
 </table>
 
 <!-- footer -->
 <?php require_once("footer.php");?>
-
